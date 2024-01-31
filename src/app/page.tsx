0b16d6b9ed3f43277b485114a488f1054e9c8d92
Jaxcoder <codenamejason@outlook.com>
2024-01-30 21:36:52 -0500

    update image/config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import { SITE_URL, FRAME_METADATA } from '@/config'
+import { FRAME_METADATA, SITE_URL } from '@/config'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   metadataBase: SITE_URL ? new URL(SITE_URL) : undefined,
-  title: 'Free mint!',
+  title: 'Fund Public Goods',
   other: FRAME_METADATA,
 }
 
 export default function Home() {
   return (
     <div style={{ minHeight: '100dvh', display: 'flex' }}>
-      <h1 style={{ margin: 'auto' }}>Free mint!</h1>
+      <h1 style={{ margin: 'auto' }}>Fund Public Goods 🌱</h1>
     </div>
   )
 }
--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
-import { base } from 'viem/chains';
 import { getFrameMetadata } from '@coinbase/onchainkit';
+import { base } from 'viem/chains';
 
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
 
 export const CHAIN = base;
-export const CONTRACT_ADDRESS = '0xfac759cefe113e510d495e8ebefa1fa7b1c7b338';
+export const CONTRACT_ADDRESS = '0xA92E003043FD043F958aBc9453a855733e98D015';
 export const TOKEN_ID = 1n; // First collection is 1
 
 export const FRAME_METADATA = getFrameMetadata({
-  buttons: ['Free mint'],
+  buttons: ['Fund Public Goods 🌱'],
   image: `${SITE_URL}/opengraph-image.png`,
   post_url: `${SITE_URL}/api/frame`,
 });
