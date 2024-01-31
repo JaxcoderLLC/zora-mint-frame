8505dc041a89149a45cc6084f29aec6a729b082c
Jaxcoder <codenamejason@outlook.com>
2024-01-31 11:43:51 -0500

    smol updates

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import { base } from 'viem/chains';
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
 
 export const CHAIN = base;
-export const CONTRACT_ADDRESS = '0xA92E003043FD043F958aBc9453a855733e98D015';
+export const CONTRACT_ADDRESS = '0x1ce20b39466bb822510a3b30bbd175396589f79b';
 export const TOKEN_ID = 1n; // First collection is 1
 
 export const FRAME_METADATA = getFrameMetadata({
